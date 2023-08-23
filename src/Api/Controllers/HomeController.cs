85d5a2ba7981e1d3e9132dedf48fc30849eef8e7
CodeSysteme <co26teme@gmail.com>
2023-08-23 23:24:26 +0100

    clean project structure

---
+++ src/Api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TuiFly.Turnover.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/Api/Extensions/ConfigureServices.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TuiFly.Turnover.Api.Extensions
+{
+    /// <summary>
+    /// Configrure and Inject all D.I of app
+    /// </summary>
+    public static class ConfigureServices
+    {
+        /// <summary>
+        /// config D.I
+        /// </summary>
+        /// <param name="service"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddAppServices(this IServiceCollection service)
+        {
+            return service;
+        }
+    }
+}
