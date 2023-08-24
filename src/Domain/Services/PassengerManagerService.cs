e4bdfb5de68fb5323f6c1cba1243bad811d0bdeb
CodeSysteme <co26teme@gmail.com>
2023-08-25 00:52:20 +0100

    add more update

--- src/Domain/Services/PassengerManagerService.cs
+++ src/Domain/Services/PassengerManagerService.cs
@@ -1,6 +1,40 @@
-﻿namespace TuiFly.Turnover.Domain.Services
+﻿using TuiFly.Turnover.Domain.Common;
+using TuiFly.Turnover.Domain.Models;
+
+namespace TuiFly.Turnover.Domain.Services
 {
+    /// <summary>
+    /// the passenger manager service
+    /// </summary>
     public class PassengerManagerService
     {
+        /// <summary>
+        /// Generate valid passengers list and determine price of each pasenger
+        /// </summary>
+        /// <param name="rawPassengers">A raw passenger file</param>
+        /// <returns></returns>
+        /// <see cref="Interfaces.IRawPassengersService"/>
+        public static IEnumerable<Passenger> GeneratePassengersList(IEnumerable<RawPassenger> rawPassengers)
+        {
+            if (!rawPassengers.Any())
+                return Array.Empty<Passenger>();
+
+            return rawPassengers.Select(p => ToPassenger(p)).OrderBy(p => p.Family).ToList();
+
+            static Passenger ToPassenger(RawPassenger rawPassenger)
+            {
+                var isOversize = rawPassenger.Places.Equals(Constants.TWO_PLACES, StringComparison.Ordinal);
+                var price = isOversize ? Constants.OVERSIZE_PRICE : rawPassenger.Age < 12 ? Constants.ENFANT_PRICE : Constants.ADULTE_PRICE;
+
+                return new Passenger
+                {
+                    Age = rawPassenger.Age,
+                    Family = rawPassenger.Famille,
+                    Type = rawPassenger.Type,
+                    OverSize = isOversize,
+                    Price = price
+                };
+            }
+        }
     }
 }
---
+++ tests/Domain/Services/PassengerManagerServiceTests.cs
@@ -0,0 +1,92 @@
+﻿using FluentAssertions;
+using TuiFly.Turnover.Domain.Common;
+using TuiFly.Turnover.Domain.Models;
+using TuiFly.Turnover.Domain.Services;
+
+namespace TuiFly.Turnover.Domain.Tests.Unit.Services
+{
+    public class PassengerManagerServiceTests
+    {
+        [Fact]
+        public void GeneratePassengersList_should_generate_empty_list()
+        {
+            //Actual
+            var resultList = PassengerManagerService.GeneratePassengersList(new List<RawPassenger>());
+
+            //Assert
+            Assert.NotNull(resultList);
+            Assert.IsType<Passenger[]>(resultList);
+            Assert.Empty(resultList);
+        }
+
+        [Theory]
+        [MemberData(nameof(ToGetRawPassengersListMock))]
+        public void GeneratePassengersList_should_generate_valid_list_of_passengers(List<RawPassenger> rawPassengers, List<Passenger> expectedResult)
+        {
+            //Actual
+            var resultList = PassengerManagerService.GeneratePassengersList(rawPassengers);
+
+            //Assert
+            Assert.NotNull(resultList);
+            Assert.IsType<List<Passenger>>(resultList);
+            resultList.Should().BeEquivalentTo(expectedResult);
+        }
+
+        public static IEnumerable<object[]> ToGetRawPassengersListMock => new List<object[]>
+        {
+            new object[] {
+                new List<RawPassenger>
+                {
+                    new RawPassenger
+                    {
+                        Age = 12,
+                        Famille = "E",
+                        Places = Constants.ONE_PLACE,
+                        Type = PassengerTypeEnum.Adulte,
+                    },
+                    new RawPassenger
+                    {
+                        Age = 45,
+                        Famille = "-",
+                        Places = Constants.TWO_PLACES,
+                        Type = PassengerTypeEnum.Adulte
+                    },
+                    new RawPassenger
+                    {
+                        Age = 7,
+                        Famille = "E",
+                        Places = Constants.ONE_PLACE,
+                        Type = PassengerTypeEnum.Enfant
+                    }
+                },
+                new List<Passenger>
+                {
+                    new Passenger
+                    {
+                        Age = 12,
+                        Family = "E",
+                        Type = PassengerTypeEnum.Adulte,
+                        Price = Constants.ADULTE_PRICE
+                    },
+                    new Passenger
+                    {
+                        Age = 45,
+                        Family = "-",
+                        OverSize = true,
+                        Type = PassengerTypeEnum.Adulte,
+                        Price = Constants.OVERSIZE_PRICE
+                    },
+                    new Passenger
+                    {
+                        Age = 7,
+                        Family = "E",
+                        Type = PassengerTypeEnum.Enfant,
+                        Price = Constants.ENFANT_PRICE
+                    }
+                },
+            }
+        };
+
+
+    }
+}
